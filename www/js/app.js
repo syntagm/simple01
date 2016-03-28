18cf986f7a3431addbc1ee61cac93a47ca67b4ee
syntagm <syntagm@icon.co.za>
2016-03-28 14:59:01 +0200

    Upload0328_03
    new js and changes to index

---
+++ www/js/app.js
@@ -0,0 +1,31 @@
+function publish() {
+
+    pubnub = PUBNUB({
+        publish_key   : 'pub-c-aecc91c5-160e-411a-8773-3a22171f396a',
+        subscribe_key : 'sub-c-84105db0-f4d1-11e5-8cfb-0619f8945a4f'
+    })
+
+    console.log("Subscribing..");
+    pubnub.subscribe({
+        channel : "hello_world",
+        message : function (message, envelope, channelOrGroup, time, channel) {
+          console.log(
+          "Message Received." + "\n" +
+          "Channel or Group: " + JSON.stringify(channelOrGroup) + "\n" +
+          "Channel: " + JSON.stringify(channel) + "\n" +
+          "Message: " + JSON.stringify(message) + "\n" +
+          "Time: " + time + "\n" +
+          "Raw Envelope: " + JSON.stringify(envelope)
+        )},
+        connect: pub
+    })
+
+    function pub() {
+       console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
+       pubnub.publish({
+            channel : "hello_world",
+            message : "Hello from PubNub Docs!",
+            callback: function(m){ console.log(m) }
+       })
+    }
+};
